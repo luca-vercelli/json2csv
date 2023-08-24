3a4c74dd1896639b9a48e47df8be83af4f511f27
Luca Vercelli <luca.vercelli@aizoongroup.com>
2023-08-24 16:50:55 +0200

    better doc

--- src/main/java/it/json2csv/Main.java
+++ src/main/java/it/json2csv/Main.java
@@ -5,29 +5,34 @@ import com.beust.jcommander.ParameterException;
 
 public class Main {
     
+    public static final String PROGRAM_NAME = "json2csv";
+    public static final String VERSION = "1.0";
+
 	public static void main(String[] args) {
 		Options cliArgs  = new Options();
 		JCommander jCommander = JCommander.newBuilder()
 			.addObject(cliArgs)
 			.build();
+        jCommander.setProgramName(PROGRAM_NAME);
+
         try {
             jCommander.parse(args);
         } catch (ParameterException exception) {
             System.err.println(exception.getMessage());
-            showUsage(jCommander, -1);
+            jCommander.usage();
+            System.exit(-1);
         }
 
         if (cliArgs.isHelp()) {
-            showUsage(jCommander, 0);
+            jCommander.usage();
+            System.exit(0);
+        } else if (cliArgs.isVersion()) {
+            System.err.println(PROGRAM_NAME + " version " + VERSION);
+            System.exit(0);
+        } else {
+            Converter convert = new Converter(cliArgs);
+            convert.run();
+            System.exit(convert.getRc());
         }
-
-		Converter convert = new Converter(cliArgs);
-		convert.run();
-        System.exit(convert.getRc());
 	}
-    
-    public static void showUsage(JCommander jCommander, int rc) {
-        jCommander.usage();
-        System.exit(rc);
-    }
 }
--- src/main/java/it/json2csv/Options.java
+++ src/main/java/it/json2csv/Options.java
@@ -9,57 +9,82 @@ public class Options {
 
     @Parameter(names = {"-h", "--help"},
             help = true,
-            description = "Displays help information")
+            description = "Displays help information and exit",
+            order = 10
+    )
     private boolean help;
+
+    @Parameter(
+      names =  {"-v", "--version"},
+      description = "Print program version and exit",
+      required = false,
+      order = 20
+    )
+    private boolean version;
 
     @Parameter(
       names =  {"-o", "--output"},
       description = "Output file. If none, output will be echoed to stdout",
-      required = false
+      required = false,
+      order = 30
     )
     private String output;
 
     @Parameter(
       names =  {"-a", "--append"},
       description = "Append to output file instead of overwrite",
-      required = false
+      required = false,
+      order = 40
     )
     private boolean append = false;
 
     @Parameter(
       names =  {"-r", "--record-delimiter"},
-      description = "Field delimiter. Default '\\r\\n'",
-      required = false
+      description = "Record delimiter. Default is Windows-style EOL, '\\r\\n'",
+      required = false,
+      order = 50
     )
     private String recordDelimiter = "\r\n";
 
     @Parameter(
       names =  {"-f", "--field-delimiter"},
       description = "Field delimiter.",
-      required = false
+      required = false,
+      order = 60
     )
     private String fieldDelimiter = ",";
 
     @Parameter(
       names =  {"-q", "--quote"},
       description = "String delimiter (or quote). Must be a single character.",
-      required = false
+      required = false,
+      order = 70
     )
     private Character quote = '"';
 
     @Parameter(
       names =  {"-e", "--escape"},
       description = "Escape character (for quote character). Must be a single character.",
-      required = false
+      required = false,
+      order = 80
     )
     private Character escape = '"';
 
     @Parameter(
       names =  {"-n", "--nested-attribute-separator"},
       description = "String used to separate nested attributes in CSV header",
-      required = false
+      required = false,
+      order = 90
     )
     private String attributeSeparator = "-";
+
+    @Parameter(
+      names =  {"--max", "--max-depth"},
+      description = "Max depth for JSON objects inspection",
+      required = false,
+      order = 100
+    )
+    private Integer maxDepth;
 
     @Parameter(
       description = "<input JSON files>",
@@ -67,13 +92,6 @@ public class Options {
       variableArity = true
     )
     private List<String> files = new ArrayList<>();
-
-    @Parameter(
-      names =  {"--max", "--max-depth"},
-      description = "Max depth for objects inspection",
-      required = false
-    )
-    private Integer maxDepth;
 
     public String getOutput() {
         return output;
@@ -154,4 +172,12 @@ public class Options {
     public void setMaxDepth(Integer maxDepth) {
         this.maxDepth = maxDepth;
     }
+
+	public boolean isVersion() {
+		return version;
+	}
+
+	public void setVersion(boolean version) {
+		this.version = version;
+	}
 }
