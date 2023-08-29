01a1b1e9cbed7515276d989ba9139e96e0703205
luca-vercelli <luca.vercelli.to@gmail.com>
2023-08-29 21:44:45 +0200

    debian package

--- debian/json2csv.sh
+++ debian/json2csv.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp /usr/share/java/commons-csv.jar:/usr/share/java/commons-io.jar:/usr/share/java/javax.json.jar:/usr/share/java/jcommander.jar:/usr/share/java/json2csv.jar it.json2csv.Main "$@"
+#java -cp /usr/share/java/commons-csv.jar:/usr/share/java/commons-io.jar:/usr/share/java/javax.json.jar:/usr/share/java/jcommander.jar:/usr/share/java/json2csv.jar it.json2csv.Main "$@"
+
+java -jar /usr/share/java/json2csv.jar "$@"
