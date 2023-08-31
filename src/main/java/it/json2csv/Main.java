5bc94e4889450e076ae4b411e22d5050f9c06f66
Luca Vercelli <luca.vercelli@aizoongroup.com>
2023-08-31 08:54:19 +0200

    version 1.1

--- src/main/java/it/json2csv/Main.java
+++ src/main/java/it/json2csv/Main.java
@@ -6,7 +6,7 @@ import com.beust.jcommander.ParameterException;
 public class Main {
     
     public static final String PROGRAM_NAME = "json2csv";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
 	public static void main(String[] args) {
 		Options cliArgs  = new Options();
