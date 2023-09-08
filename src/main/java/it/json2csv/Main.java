e8ad8ccaa4fbba2c3149d02d065cd927bd1a1b2e
Luca Vercelli <luca.vercelli@aizoongroup.com>
2023-09-08 14:38:14 +0200

    file properties

--- src/main/java/it/json2csv/Main.java
+++ src/main/java/it/json2csv/Main.java
@@ -1,19 +1,23 @@
 package it.json2csv;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 
 public class Main {
-    
-    public static final String PROGRAM_NAME = "json2csv";
-    public static final String VERSION = "1.1";
 
-	public static void main(String[] args) {
-		Options cliArgs  = new Options();
-		JCommander jCommander = JCommander.newBuilder()
-			.addObject(cliArgs)
-			.build();
-        jCommander.setProgramName(PROGRAM_NAME);
+    public static void main(String[] args) {
+        Properties prop = loadProperties();
+        final String NAME = prop.getProperty("NAME");
+        final String VERSION = prop.getProperty("VERSION"); 
+
+        Options cliArgs = new Options();
+        JCommander jCommander = JCommander.newBuilder()
+                .addObject(cliArgs)
+                .build();
+        jCommander.setProgramName(NAME);
 
         try {
             jCommander.parse(args);
@@ -27,12 +31,22 @@ public class Main {
             jCommander.usage();
             System.exit(0);
         } else if (cliArgs.isVersion()) {
-            System.err.println(PROGRAM_NAME + " version " + VERSION);
+            System.err.println(NAME + " version " + VERSION);
             System.exit(0);
         } else {
             Converter convert = new Converter(cliArgs);
             convert.run();
             System.exit(convert.getRc());
         }
-	}
+    }
+
+    public static Properties loadProperties() {
+        Properties prop = new Properties();
+        try {
+            prop.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
+        } catch (IOException ex) {
+            System.err.println("WARN: application.properties not found or not readable");
+        }
+        return prop;
+    }
 }
